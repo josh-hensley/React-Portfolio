c632be26fe772b56e5e290a589f2408dec5e3745
josh-hensley <me@joshhensley.com>
2024-11-03 18:28:21 -0600

    added basic components

---
+++ client/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer(){
+    return (
+        <>
+            <ul>
+                <li>footer</li>
+                <li>stuff</li>
+            </ul>
+        </>
+    )
+}
+export default Footer;
---
+++ client/components/Nav.jsx
@@ -0,0 +1,13 @@
+function Nav(){
+    return (
+        <>
+            <ul>
+                <li>Nav 1</li>
+                <li>Nav 2</li>
+                <li>Nav 3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Nav;
