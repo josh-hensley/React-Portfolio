5ebe58025b79e3bc376fb7c02a079185053ec500
josh-hensley <me@joshhensley.com>
2024-11-05 15:16:29 -0600

    semblance of resume page created

--- client/src/components/Content/Resume.jsx
+++ client/src/components/Content/Resume.jsx
@@ -1,41 +1,42 @@
 function Resume() {
     return (
         <>
-            <div className="container text-center">
-                <h1>Josh Hensley</h1>
-                <div className="row">
-                    <div className="col"></div>
-                    <div className="card col">
-                        <div className="card-body">
-                            <h5 className="card-title">Education</h5>
-                            <div className="card">
-                                <div className="card-body">
-                                    <p className="card-text">Tarleton State University - BFA Theatre</p>
-                                    <figcaption>August 2009-August 2013</figcaption>
-                                </div>
+            <div className="row">
+                <div className="col"></div>
+                <div className="col">
+                    <div className="container">
+                    <h1 className="text-center">Josh Hensley's Resumé</h1>
+                    <div className="card m-5 text-center">
+                        <h5 className="card-title my-1 text-center">Education</h5>
+                        <hr />
+                        <h6 className="card-text">Tarleton State University - BFA Theatre</h6>
+                        <h6 className="card-text">SMU fullstack developement bootcamp</h6>
+                    </div>
+                    <div className="card m-5">
+                        <h5 className="card-title my-1 text-center">Experience</h5>
+                        <hr />
+                        <div className="row">
+                            <div className="col text-end">
+                                <h6 className="card-text">2019-Present</h6>
+                                <h6 className="card-text">2021-Present</h6>
+                                <h6 className="card-text">2020-2023</h6>
+                                <h6 className="card-text">2015-2020</h6>
+                                <h6 className="card-text">2013-Present</h6>
+                                <h6 className="card-text">2016-2019</h6>
                             </div>
-                            <div className="card">
-                                <div className="card-body">
-                                    <p className="card-text">SMU fullstack developement bootcamp</p>
-                                    <figcaption>August 2024-January 2025</figcaption>
-                                </div>
+                            <div className="col">
+                                <h6 className="card-text">Stage Corps</h6>
+                                <h6 className="card-text">Repertory Company Theatre</h6>
+                                <h6 className="card-text">IMPRINT Theatreworks</h6>
+                                <h6 className="card-text">Garland Civic Theatre</h6>
+                                <h6 className="card-text">Independant Freelance Theatre Artist</h6>
+                                <h6 className="card-text">Naaman Forest High School</h6>
                             </div>
                         </div>
                     </div>
-                    <div className="col"></div>
-                </div>
-                <div className="row">
-                    <div className="col"></div>
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Experience</h5>
-                                
-                            </div>
-                        </div>
                     </div>
-                    <div className="col"></div>
                 </div>
+                <div className="col"></div>
             </div>
         </>
     )
