e2f66db1b82145ccda3bd53fe9026a20c6ab9c0a
josh-hensley <me@joshhensley.com>
2024-11-05 18:18:30 -0600

    portfolio page created and scaffolded

--- client/src/components/Content/Portfolio.jsx
+++ client/src/components/Content/Portfolio.jsx
@@ -1,8 +1,93 @@
-function Portfolio(){
+function Portfolio() {
     return (
         <>
-            <div>
-                <p>Portfolio Goes Here.</p>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-lg-4">
+                        <div className="card">
+                            <img src="/vite.svg" alt="" className="card-img-top align-self-center" style={{ width: '200px' }} />
+                            <div className="card-body">
+                                <h5 className="card-title">Project 1</h5>
+                                <p className="card-text">
+                                    <a href="">Deployed</a>
+                                </p>
+                                <p className="card-text">
+                                    <a href="">Repo</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="card">
+                            <img src="/vite.svg" alt="" className="card-img-top align-self-center" style={{ width: '200px' }} />
+                            <div className="card-body">
+                                <h5 className="card-title"> Project 2 </h5>
+                                <p className="card-text">
+                                    <a href="">Deployed</a>
+                                </p>
+                                <p className="card-text">
+                                    <a href="">Repo</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="card">
+                            <img src="/vite.svg" alt="" className="card-img-top align-self-center" style={{ width: '200px' }} />
+                            <div className="card-body">
+                                <h5 className="card-title">Project 3</h5>
+                                <p className="card-text">
+                                    <a href="">Deployed</a>
+                                </p>
+                                <p className="card-text">
+                                    <a href="">Repo</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="card">
+                            <img src="/vite.svg" alt="" className="card-img-top align-self-center" style={{ width: '200px' }} />
+                            <div className="card-body">
+                                <h5 className="card-title">Project 4</h5>
+                                <p className="card-text">
+                                    <a href="">Deployed</a>
+                                </p>
+                                <p className="card-text">
+                                    <a href="">Repo</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="card">
+                            <img src="/vite.svg" alt="" className="card-img-top align-self-center" style={{ width: '200px' }} />
+                            <div className="card-body">
+                                <h5 className="card-title">Project 5</h5>
+                                <p className="card-text">
+                                    <a href="">Deployed</a>
+                                </p>
+                                <p className="card-text">
+                                    <a href="">Repo</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="card">
+                            <img src="/vite.svg" alt="" className="card-img-top align-self-center" style={{ width: '200px' }} />
+                            <div className="card-body">
+                                <h5 className="card-title">Project 6</h5>
+                                <p className="card-text">
+                                    <a href="">Deployed</a>
+                                </p>
+                                <p className="card-text">
+                                    <a href="">Repo</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
