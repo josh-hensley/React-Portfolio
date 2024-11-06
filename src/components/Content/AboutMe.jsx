69195f6c49c8e71c263453792214e290baf2b135
josh-hensley <me@joshhensley.com>
2024-11-05 21:52:25 -0600

    fixed linting errors

--- src/components/Content/AboutMe.jsx
+++ src/components/Content/AboutMe.jsx
@@ -11,13 +11,13 @@ function AboutMe() {
                         <p className='card-text my-5'>
                             Josh began his professional career in theatre at Garland Summer Musicals. The Granville Arts
                             Center would become a bit of a summer home for him, trying out many hats along the way. Those
-                            "hats" included actor, master carpenter, master electrician, and assistant stage manager. After
+                            &quot;hats&quot; included actor, master carpenter, master electrician, and assistant stage manager. After
                             graduating from Tarleton State University with a fever for directing, Mr. Hensley set out to
                             find a theatre that would allow him to refine his skills. One such opportunity revealed itself
-                            as Art Centre Theatre's production of Evil Dead the Musical. The production was fully directed 
-                            and designed by Josh Hensley and was later nominated for Dallas Observer's "Best of Dallas" Reader's 
+                            as Art Centre Theatre&apos;s production of Evil Dead the Musical. The production was fully directed 
+                            and designed by Josh Hensley and was later nominated for Dallas Observer&apos;s &quot;Best of Dallas&quot; Reader&apos;s 
                             Choice for Best Musical. With his production of Murder Ballad, Hensley had convinced Artistic Director
-                             Kyle McClaren that his style would work well with Garland Civic Theatre's vision and thus found a new 
+                             Kyle McClaren that his style would work well with Garland Civic Theatre&apos;s vision and thus found a new 
                              home for many years. Directing shows such as Watson: The Last Great Tale of the Legendary Sherlock Holmes,
                               Dial M for Murder, The 39 Steps, and the US Premiere of The Widow.<hr/>
 
@@ -26,7 +26,7 @@ function AboutMe() {
                             the Area competition, a first for Naaman Forest. In March 2020, at the beginning of the Pandemic, Josh would 
                             serve as Technical Manager for IMPRINT Theatreworks. His work could be seen in their many exciting productions 
                             such as, The Impact of the Gadget on Civilization and Pussy Riot! or Everything is P.R. Sadly, IMPRINT would 
-                            close it's doors in 2022. Hensley is now the Technical Director for Theatre Frisco and a regular Lighting Designer
+                            close it&apos;s doors in 2022. Hensley is now the Technical Director for Theatre Frisco and a regular Lighting Designer
                              for Repertory Company Theatre.</p>
                     </div>
                     <div className="col-md-2 col-lg"></div>
